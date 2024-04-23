81af678d493fec5be902310e2ae92a0405285f01
Grav <grafimaulanas@gmail.com>
2024-04-23 17:14:14 +0700

    add files

--- resources/js/Pages/Test.vue
+++ resources/js/Pages/Test.vue
@@ -1,25 +1,38 @@
 <script setup>
 import { Head, Link } from '@inertiajs/vue3';
 
-defineProps({
-    canLogin: {
-        type: Boolean,
-    },
-    canRegister: {
-        type: Boolean,
-    },
-    laravelVersion: {
-        type: String,
-        required: true,
-    },
-    phpVersion: {
-        type: String,
-        required: true,
-    },
-});
+const message ="";
+const weat = {
+    query: '',
+    weather: {},
+    url_base: "https://cuaca-gempa-rest-api.vercel.app/weather/",
+    // weather_icon: "http://openweathermap.org/img/wn/",
+};
+
+const fetchWeather = (e) => {
+    // if (e.key == "Enter"){
+    //     let response = axios.get(
+            
+    //     )
+    // }
+}
 </script>
 
 <template>
     <Head title=""/>
-        <h1>this test page</h1>
+    <h1>{{ weat.query }} asd</h1>
+    <div class="weather-container">
+        <div class="weather-wrap">
+            <div class="search-box">
+                <input type="text" placeholder="Search..." class="search-bar" 
+                v-model="weat.query"/>
+            </div>
+            <p>Message is: {{ message }}</p>
+            <input v-model="message" placeholder="edit me" />
+            <!-- <div class="weather-info" v-if="typeof weather.main!=undefined'">
+                <div class="location-box">
+                <div class="location">{{weather.name}},{{weather.sys.country }}
+            </div> -->
+        </div>
+    </div>
 </template>
