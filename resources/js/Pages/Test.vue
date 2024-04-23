2c339e4bed9ad5ad2e9f9e927a441eae854d7eb7
Grav <grafimaulanas@gmail.com>
2024-04-23 17:55:52 +0700

    update files

--- resources/js/Pages/Test.vue
+++ resources/js/Pages/Test.vue
@@ -1,38 +1,27 @@
 <script setup>
 import { Head, Link } from '@inertiajs/vue3';
+import { ref } from 'vue'
 
 const message ="";
+const query = ref('');
 const weat = {
-    query: '',
     weather: {},
     url_base: "https://cuaca-gempa-rest-api.vercel.app/weather/",
     // weather_icon: "http://openweathermap.org/img/wn/",
 };
 
-const fetchWeather = (e) => {
-    // if (e.key == "Enter"){
-    //     let response = axios.get(
-            
-    //     )
-    // }
-}
+
 </script>
 
 <template>
     <Head title=""/>
-    <h1>{{ weat.query }} asd</h1>
     <div class="weather-container">
         <div class="weather-wrap">
             <div class="search-box">
-                <input type="text" placeholder="Search..." class="search-bar" 
-                v-model="weat.query"/>
+                <input type="text" placeholder="Search" class="search-bar" 
+                v-model="query"/>
             </div>
-            <p>Message is: {{ message }}</p>
-            <input v-model="message" placeholder="edit me" />
-            <!-- <div class="weather-info" v-if="typeof weather.main!=undefined'">
-                <div class="location-box">
-                <div class="location">{{weather.name}},{{weather.sys.country }}
-            </div> -->
+            <h1>Kota : {{ query }}</h1>
         </div>
     </div>
 </template>
