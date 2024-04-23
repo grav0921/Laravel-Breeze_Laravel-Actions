e657bd7871b86e1e15bda5a5fa99988a82ea43dd
Grav <grafimaulanas@gmail.com>
2024-04-23 09:24:03 +0700

    laravel commit

---
+++ app/Actions/Register.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Actions;
+
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+use Inertia\Inertia;
+use Inertia\Response;
+use Illuminate\Support\Facades\Validator;
+
+
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class Register
+{
+    use AsAction;
+
+    public function handle(string $name, string $email, string $password)
+    {
+        $user = User::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => Hash::make($password),
+        ]);
+
+        event(new Registered($user));
+
+        Auth::login($user);
+
+       return $user;
+    }
+}
