78ec5e5b23f114650d6129babfaafeadf762db72
Grav <grafimaulanas@gmail.com>
2024-04-24 12:58:19 +0700

    update files

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/test', function () {
+Route::get('/weather', function () {
     return Inertia::render('Test');
 });
 
